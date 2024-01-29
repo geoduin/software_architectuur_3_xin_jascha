204142394d2e7cb07024ae0144f8ff3a395aec01
xin <xin20wang@outlook.com>
2024-01-29 12:09:03 +0100

    Pushed domain classes

---
+++ domain/MovieScreening.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace software_architectuur_3_xin_jascha.domain
+{
+    internal class MovieScreening
+    {
+    }
+}
