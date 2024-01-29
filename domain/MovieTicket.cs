ebf58435e1974f850f464979873a1ab6a84f3bea
Jascha <jascha3600@gmail.com>
2024-01-29 12:43:02 +0100

    Added member variables and methods to MovieTicket class

--- domain/MovieTicket.cs
+++ domain/MovieTicket.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace software_architectuur_3_xin_jascha.domain
 {
+
     internal class MovieTicket
     {
+        private int RowNr {  get; set; }
+        private int SeatNr { get; set; }
+        private bool IsPremium { get; set; }
+
+        public MovieTicket(MovieScreening movieScreening, bool IsPremiumReservation, int SeatRow, int SeatNr)
+        {
+
+        }
+
+        public bool IsPremiumTicket()
+        {
+            return this.IsPremium;
+        }
+
+        public double GetPrice()
+        {
+            return -1;
+        }
+
+        public String ToString()
+        {
+            return "henk";
+        }
     }
 }
