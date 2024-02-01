de1717561512037f5bbbb92982bb74251d9d09f3
Xin Wang <xx.wang@student.avans.nl>
2024-02-01 10:16:35 +0100

    Temp remove test

--- Program.cs
+++ Program.cs
@@ -2,10 +2,3 @@
 using software_architectuur_3_xin_jascha.domain;
 
 Console.WriteLine("Hello, World!");
-
-Order Test = new Order(1, true);
-var Movie = new Movie("Steamboat Willie");
-var Screening = new MovieScreening(Movie, DateTime.Now, 10.50);
-var Ticket = new MovieTicket(Screening, false, 1, 1);
-Test.AddSeatReservation(Ticket);
-Test.Export(TicketExportFormat.JSON);
