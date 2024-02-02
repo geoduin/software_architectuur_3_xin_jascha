30a8a7e2e50742b74d14ad751f44cb80f6e1989c
Jascha <jascha3600@gmail.com>
2024-02-02 11:18:30 +0100

    Henk

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using software_architectuur_3_xin_jascha.domain;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 Order Test = new Order(1, true);
 var Movie = new Movie("Steamboat Willie");
